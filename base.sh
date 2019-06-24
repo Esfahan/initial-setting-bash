ef10267671ca180d17e08d5d519e58fce403f075
Esfahan <calito0131@gmail.com>
2019-06-24 16:57:31 +0900

    Separated the installer script

---
+++ base.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# set timezone
+echo "----------------------"
+echo "set timezone as Japan"
+echo "----------------------"
+cmd="sudo cp /usr/share/zoneinfo/Japan /etc/localtime"
+echo $cmd
+$cmd
+date
+
+# vim
+echo "----------------------"
+echo "install vim-enhanced"
+echo "----------------------"
+sudo yum -y install vim-enhanced
+sudo mv /bin/vi /bin/vim-tiny
+sudo ln -s /usr/bin/vim /bin/vi
+
+# vimrc
+echo "----------------------"
+echo "edit ~/.vimrc"
+echo "----------------------"
+if [ ! -e ~/.vimrc ]; then
+  touch ~/.vimrc
+  echo 'touch ~/.vimrc'
+fi
+
+ADDED_COMMENT='added my settings for vim'
+cat ~/.vimrc | grep -w "${ADDED_COMMENT}" > /dev/null 2>&1
+if [ $? = 1 ]; then
+  cat ./dotfiles/vim/vimrc >> ~/.vimrc
+  echo 'added.'
+
+  echo "----------------------"
+  echo "copy ~/.vim/ftplugin"
+  echo "----------------------"
+  if [ ! -d ~/.vim/ftplugin/ ]; then
+    mkdir -p ~/.vim/ftplugin/
+    echo 'mkdir -p ~/.vim/ftplugin/'
+  fi
+
+  cp -p ./dotfiles/vim/ftplugin/* ~/.vim/ftplugin/
+  echo 'added.'
+
+else
+  echo 'alreday exists.'
+fi
+
+# bash_profile
+echo "----------------------"
+echo "edit ~/.bash_profile"
+echo "----------------------"
+if [ ! -e ~/.bash_profile ]; then
+  touch ~/.bash_profile
+  echo 'touch ~/.bash_profile'
+fi
+if [ ! -e ~/.git-prompt.sh ]; then
+  touch ~/.git-prompt.sh
+  echo 'touch ~/.git-prompt.sh'
+fi
+if [ ! -e ~/.git-completion.bash ]; then
+  touch ~/.git-completion.bash
+  echo 'touch ~/.git-completion.bash'
+fi
+
+ADDED_COMMENT_BP='added my settings'
+cat ~/.bash_profile | grep -w "${ADDED_COMMENT_BP}" > /dev/null 2>&1
+if [ $? = 1 ]; then
+  # https://github.com/git/git/tree/master/contrib/completion
+  cat ./dotfiles/git/git-prompt.sh > ~/.git-prompt.sh
+  cat ./dotfiles/git/git-completion.bash > ~/.git-completion.bash
+  cat ./dotfiles/bash_profile >> ~/.bash_profile
+  cp ./dotfiles/my_profile ~/.my_profile
+  echo 'added.'
+else
+  echo 'alreday exists.'
+fi
+
+tmux list-client > /dev/null 2>&1
+if [ $? = 1 ]; then
+  cd ~
+  tmux
+fi
